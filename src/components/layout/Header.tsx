5bdbf6868605944312ea212ec9ff595493ec211c
yologger <yologger1013@gmail.com>
2025-07-13 14:55:03 +0900

    chore: configure @emotion

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,11 @@
+import styled from '@emotion/styled';
+
 const Header = () => {
-  return <div>Header</div>;
+  return <Container>Header</Container>;
 };
 
 export default Header;
+
+const Container = styled.div`
+  background-color: #aeaeff;
+`;
