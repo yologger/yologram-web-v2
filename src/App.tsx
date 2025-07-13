4d6c01472caf6b630881280d9f96ac00bb584ca1
yologger <yologger1013@gmail.com>
2025-07-13 15:20:15 +0900

    chore: apply reset css, global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { Global } from '@emotion/react';
 import Layout from './components/layout/Layout';
 import Router from './Router';
+import { global } from './styles/global';
+import { reset } from './styles/reset';
 
 const App = () => {
   return (
-    <Layout>
-      <Router />
-    </Layout>
+    <>
+      <Global styles={reset} />
+      <Global styles={global} />
+      <Layout>
+        <Router />
+      </Layout>
+    </>
   );
 };
 
---
+++ src/styles/global.ts
@@ -0,0 +1,7 @@
+import { css } from '@emotion/react';
+
+export const global = css`
+  * {
+    font-family: 'Nanum Gothic', sans-serif;
+  }
+`;
